08eae6247642db33aa35ae351fb24e8d9994e3a1
Ayush Kumar <kumarayush9651@tutanota.com>
2026-08-05 10:09:04 +0530

    Added notes for two pointer

---
+++ two_pointers/two_ptr.cpp
@@ -0,0 +1,16 @@
+// Same direction pass -> Fast and slow pointer
+//  -Can detect middle of linked lists (ds of unknown size)
+//  -Can find the loop like in linked list
+
+
+
+// Diff direction pass
+//  -Usually work with sorted arrays
+//  -Problems like palindrome
+
+
+//Problems where 2 ptr is required
+// -merging sorted data
+// -palindromes
+// -Reversals
+// -k sized comparisons
